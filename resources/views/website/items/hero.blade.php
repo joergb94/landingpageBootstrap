070f903c6cacdd93b772a24ac3f008dc7f3fe1bc
Jose Gamboa <67659588+joergb94@users.noreply.github.com>
2024-01-13 13:42:29 -0600

    add changes on page 1

--- resources/views/website/items/hero.blade.php
+++ resources/views/website/items/hero.blade.php
@@ -1,19 +1,15 @@
-<section id="hero" class="d-flex align-items-center">
+<section id="hero" class="d-flex align-items-center" style="background-image:url('{{asset('assets/img/background/1.png')}}'); background-repeat: no-repeat; background-size:cover; background-size: 100% 100%;">
 
-<div class="container">
-  <div class="row">
-    <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-      <h1>Better Solutions For Your Business</h1>
-      <h2>We are team of talented designers making websites with Bootstrap</h2>
-      <div class="d-flex justify-content-center justify-content-lg-start">
-        <a href="#about" class="btn-get-started scrollto">Get Started</a>
-        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+  <div class="hero-content container">
+    <div class="row">
+      <div class="col-sm-12 d-flex align-items-center flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>Soluciones <b>únicas</b> y</h1>
+          <h1><b>flexibles</b> a tu alcance.</h1>
+        <div class="hero-div d-flex justify-content-center justify-content-lg-start">
+          <a href="#about" class="btn btn-get-started scrollto">Contactanos</a>
+        </div>
       </div>
     </div>
-    <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-      <img src="{{asset('assets/img/hero-img.png')}}" class="img-fluid animated" alt="">
-    </div>
   </div>
-</div>
 
 </section>
