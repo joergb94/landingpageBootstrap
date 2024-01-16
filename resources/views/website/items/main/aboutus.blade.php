5bdfec65002757eb31e744c25a1d1db6bbef9ea9
Jose Gamboa <67659588+joergb94@users.noreply.github.com>
2024-01-15 18:52:48 -0600

    add section vision

--- resources/views/website/items/main/aboutus.blade.php
+++ resources/views/website/items/main/aboutus.blade.php
@@ -1,29 +1,72 @@
-<section id="about" class="about">
+<section id="about" class="services section-bg" style="background-image:url('{{asset('assets/img/background/2.png')}}'); background-repeat: no-repeat; background-size:cover; background-size: 100% 100%;">
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
-          <h2>About Us</h2>
+          <h2>Services</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
 
-        <div class="row content">
-          <div class="col-lg-6">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
-            </p>
-            <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-            </ul>
+        <div class="row  d-flex align-items-center flex-column justify-content-center">
+          <div class="col-sm-10" data-aos="zoom-in" data-aos-delay="100">
+           <div class="row justify-content-center">
+            <div class="icon-box col-sm-8" style="color:white; background-color:#514e79;">
+                    <div class="row">
+                      <div class="col-sm-4">
+                        <img src="{{asset('assets/img/icons/vision.png')}}" class="img-fluid mx-auto d-block">
+                      </div>
+                      <div class="col-sm-8">
+                        <h4>VISION</h4>
+                        <p>Ir más allá de lo convencional para
+                          convertirnos en el socio
+                          tecnológico de negocios de
+                          nuestros clientes mediante nuestra
+                          capacidad y compromiso
+                          inquebrantable para potenciar,
+                          expandir y perfeccionar el
+                          funcionamiento de su empresa o
+                          entidad.
+                        </p>
+                      </div>
+                    </div>
+                  
+                </div>
+              <div class="col-sm-1 justify-content-center">
+                <button class="btn btn-primary rounded-circle" style="margin-top: 150px; width:50px; height:50px; color:white; background-color:#3D3868;" > 
+                  <div class="icon"><i class="bi bi-caret-right-fill"></i> </div>
+                </button>
+              </div>
+           </div>
           </div>
-          <div class="col-lg-6 pt-4 pt-lg-0">
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a href="#" class="btn-learn-more">Learn More</a>
+
+          <div class="col-sm-10 " data-aos="zoom-in" data-aos-delay="100">
+           <div class="row justify-content-center">
+              <div class="col-sm-1 justify-content-center">
+                <button class="btn btn-primary rounded-circle" style="margin-top: 150px; width:50px; height:50px; color:white; background-color:#3D3868;" > 
+                  <div class="icon"><i class="bi bi-caret-left-fill"></i> </div>
+                </button>
+              </div>
+              <div class="icon-box col-sm-8" style="color:white; background-color:#514e79;">
+                  <div class="row">
+                    <div class="col-sm-4">
+                      <img src="{{asset('assets/img/icons/vision.png')}}" class="img-fluid mx-auto d-block">
+                    </div>
+                    <div class="col-sm-8">
+                      <h4>VISION</h4>
+                      <p>Ir más allá de lo convencional para
+                        convertirnos en el socio
+                        tecnológico de negocios de
+                        nuestros clientes mediante nuestra
+                        capacidad y compromiso
+                        inquebrantable para potenciar,
+                        expandir y perfeccionar el
+                        funcionamiento de su empresa o
+                        entidad.
+                      </p>
+                    </div>
+                  </div>
+                 
+              </div>
+           </div>
           </div>
         </div>
 
